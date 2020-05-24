81516627af54ff34d1a0320911843e79ad60002a
rares <colorlord98@gmail.com>
2020-05-24 21:45:00 +0300

    Removed comment from rankedstatus class

--- objects/rankedstatus.py
+++ objects/rankedstatus.py
@@ -10,19 +10,3 @@ class RankedStatus(Enum):
 	APPROVED = 5
 	QUALIFIED = 6
 	LOVED = 7
-'''
-	def __str__(self):
-		print(self.value)
-		if self.value == RankedStatus.UNKNOWN:
-			return "UNKNOWN"
-		if self.value in [RankedStatus.UNSUBMITTED, RankedStatus.PENDING_WIP_GRAVEYARD, RankedStatus.UNUSED]:
-			return "UNRANKED"
-		if self.value == RankedStatus.RANKED:
-			return "RANKED"
-		if self.value == RankedStatus.APPROVED:
-			return "APPROVED"
-		if self.value == RankedStatus.QUALIFIED:
-			return "QUALIFIED"
-		if self.value == RankedStatus.LOVED:
-			return "LOVED"
-'''
